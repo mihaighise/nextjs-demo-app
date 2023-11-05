1454e2fced610c5eca77d252aca25a9dcc192a7f
mihaighise <mihaighise1@gmail.com>
2023-11-05 15:04:10 +0200

    Build API to fetch issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -20,3 +20,8 @@ export async function POST(request: NextRequest) {
 
   return NextResponse.json(newIssue, { status: 201 });
 }
+
+export async function GET() {
+  const issues = await prisma.issue.findMany();
+  return NextResponse.json(issues);
+}
