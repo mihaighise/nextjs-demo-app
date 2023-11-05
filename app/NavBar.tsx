b13e82e0f406a05714ecb30f62901315005e0dbd
mihaighise <mihaighise1@gmail.com>
2023-11-05 14:38:17 +0200

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,24 +1,33 @@
+'use client'
+import classnames from 'classnames'
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 import { AiFillBug } from 'react-icons/ai'
 
 const NavBar = () => {
+    const currentPath = usePathname();
+
     const links  = [
-        { label: 'Dashboard', href: '/'},
-        { label: 'Issues', href: '/issues'}
+        { label: 'Dashboard', href: "/"},
+        { label: 'Issues', href: "/issues"}
     ]
 
-  return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-        <Link href="/"><AiFillBug></AiFillBug></Link>
-        <ul className="flex space-x-6">
-            { links.map(link => 
-                <Link key={link.label} 
-                    className="text-zinc-500 hover:text-zinc-800 transition-colors" 
-                    href={link.href}
-                >{link.label}</Link>)}
-        </ul>
-    </nav>
-  )
+    return (
+        <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+            <Link href="/"><AiFillBug></AiFillBug></Link>
+            <ul className="flex space-x-6">
+                { links.map(link => 
+                    <Link key={link.label} 
+                        className={classnames({
+                            'text-slate-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}
+                        href={link.href}
+                    >{link.label}</Link>)}
+            </ul>
+        </nav>
+    )
 }
 
 export default NavBar
